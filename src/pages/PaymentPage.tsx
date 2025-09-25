3dc15c063c1f9f42717f1cf60a3a9a6fda668a80
Aditya K <adityakaklij11@gmail.com>
2025-09-25 10:11:25 +0530

    cashfreer to production

--- src/pages/PaymentPage.tsx
+++ src/pages/PaymentPage.tsx
@@ -142,8 +142,8 @@ export default function PaymentPage() {
     const initializeSDK = async () => {
       try {
         const cfSDK = await load({
-          mode: "sandbox" // Use production mode
-          // mode: "production" // Use production mode
+          // mode: "sandbox" // Use production mode
+          mode: "production" // Use production mode
         });
         setCashfreeSDK(cfSDK);
       } catch (error) {
